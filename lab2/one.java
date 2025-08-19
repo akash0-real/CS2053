c3cd76c84ba3f10835236107db37fe4aee048eb2
Akash bisht <akashsinghbishtbca24@rvu.edu.in>
2025-08-19 10:56:26 +0530

    inhiertence

---
+++ lab2/one.java
@@ -0,0 +1,18 @@
+class Animal{
+    void sound(){
+        System.out.println("Animla makes a sound!");
+
+    }
+}
+public class one{
+    void sound(){
+        System.out.println("Dog barks woof woof!");
+    }
+    public static void main(String[] args){
+            Animal A = new Animal();
+            A.sound();
+
+
+    }
+    
+}
